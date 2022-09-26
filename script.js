174d6c135d8bc2c1f8c9e2f8aeb941c0ceb2479e
cole0404 <coletonboles2004@gmail.com>
2022-09-26 09:02:55 -0500

    Moved console logs to for loop to show round results

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-
-let computerSelection = getComputerChoice();
-
 let player = 0;
 let computer = 0;
 let draw = 0;
@@ -21,42 +18,42 @@ function getPlayerChoice() {
 }
 
 function playRound(playerSelection, computerSelection) {
-    if (playerSelection == computerSelection) return tie;
+    if (playerSelection == computerSelection) ++draw;
 
     else if (playerSelection == "rock" && computerSelection == "scissors"
         || playerSelection == "scissors" && computerSelection == "paper"
         || playerSelection == "paper" && computerSelection == "rock"
     ) 
-    return win;
+    ++ player;
 
     else if (playerSelection == "rock" && computerSelection == "paper"
         || playerSelection == "scissors" && computerSelection == "rock"
         || playerSelection == "paper" && computerSelection == "scissors"
     ) 
-    return lose;
+    ++ computer;
 
     else return "Please enter a valid choice.";
 }
 
-function game(playerSelection, computerSelection) {
+function game() {
 
     for (let i = 0; i < 5; i++) {
+       
+        let computerSelection = getComputerChoice();
         let playerSelection = getPlayerChoice();
-        let round = playRound();
 
         playRound(playerSelection, computerSelection);
-         
-         if (round = win) ++ player;
 
-         else if (round = lose) ++ computer;
-     
-         else if (round = tie) ++ draw;
-     }
+        console.log("You picked " + playerSelection + " and the computer chose " + computerSelection);
+        console.log("Player score: " + player);
+        console.log("Computer score: " + computer);
+        console.log("Draws: " + draw);
+    }
+    if (player > computer) return win;
+        
+    else if (computer > player) return lose;
 
-     
+    else return tie; 
 }
-console.log(computerSelection);
+
 console.log(game());
-console.log(player);
-console.log(computer);
-console.log(draw);
